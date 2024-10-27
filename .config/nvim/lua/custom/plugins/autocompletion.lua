d48507369445f88d5744c8f7d51b9d917b92d1cd
lebedevv <sintrael@gmail.com>
2024-10-27 14:02:18 +0300

    nvim: blink.cmp updated providers structure for new format

--- .config/nvim/lua/custom/plugins/autocompletion.lua
+++ .config/nvim/lua/custom/plugins/autocompletion.lua
@@ -112,23 +112,84 @@ return {
 			windows = {
 				documentation = { auto_show = true },
 			},
+
 			sources = {
+				-- list of enabled providers
+				completion = {
+					enabled_providers = { "lsp", "path", "snippets", "buffer" },
+				},
+
 				providers = {
-					{ "blink.cmp.sources.lsp", name = "LSP", score_offset = 1 },
-					{
-						"blink.cmp.sources.snippets",
-						name = "Snippets",
+					lsp = {
+						name = "LSP",
+						module = "blink.cmp.sources.lsp",
+
+						--- *All* of the providers have the following options available
+						--- NOTE: All of these options may be functions to get dynamic behavior
+						--- See the type definitions for more information
+						enabled = true, -- whether or not to enable the provider
+						transform_items = nil, -- function to transform the items before they're returned
+						should_show_items = true, -- whether or not to show the items
+						max_items = nil, -- maximum number of items to return
+						min_keyword_length = 0, -- minimum number of characters to trigger the provider
+						fallback_for = {}, -- if any of these providers return 0 items, it will fallback to this provider
+						score_offset = 1, -- boost/penalize the score of the items
+						override = nil, -- override the source's functions
 					},
-					{ "blink.cmp.sources.path", name = "Path", score_offset = 3, opts = { get_cwd = vim.uv.cwd } },
-					{
-						"blink.cmp.sources.buffer",
+					path = {
+						name = "Path",
+						module = "blink.cmp.sources.path",
+						score_offset = 3,
+						opts = {
+							trailing_slash = false,
+							label_trailing_slash = true,
+							get_cwd = function(context)
+								return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+							end,
+							show_hidden_files_by_default = false,
+						},
+					},
+					snippets = {
+						name = "Snippets",
+						module = "blink.cmp.sources.snippets",
+						score_offset = -3,
+						opts = {
+							friendly_snippets = true,
+							search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+							global_snippets = { "all" },
+							extended_filetypes = {},
+							ignored_filetypes = {},
+						},
+
+						--- Example usage for disabling the snippet provider after pressing trigger characters (i.e. ".")
+						-- enabled = function(ctx) return ctx ~= nil and ctx.trigger.kind == vim.lsp.protocol.CompletionTriggerKind.TriggerCharacter end,
+					},
+					buffer = {
 						name = "Buffer",
-						keyword_length = 3,
+						module = "blink.cmp.sources.buffer",
 						score_offset = -2,
-						fallback_for = { "Path" }, -- PENDING https://github.com/Saghen/blink.cmp/issues/122
+						fallback_for = { "lsp" },
 					},
 				},
 			},
+
+			-- sources = {
+			-- providers = {
+			-- 	{ "blink.cmp.sources.lsp", name = "LSP", score_offset = 1 },
+			-- 	{
+			-- 		"blink.cmp.sources.snippets",
+			-- 		name = "Snippets",
+			-- 	},
+			-- 	{ "blink.cmp.sources.path", name = "Path", score_offset = 3, opts = { get_cwd = vim.uv.cwd } },
+			-- 	{
+			-- 		"blink.cmp.sources.buffer",
+			-- 		name = "Buffer",
+			-- 		keyword_length = 3,
+			-- 		score_offset = -2,
+			-- 		fallback_for = { "Path" }, -- PENDING https://github.com/Saghen/blink.cmp/issues/122
+			-- 	},
+			-- },
+			-- },
 		},
 	},
 }
