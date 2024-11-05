ea8f63886f518ef06ddac951ea6c2a5aaf891d2d
lebedevv <sintrael@gmail.com>
2024-11-05 10:59:55 +0300

    nvim: simplify harpoon bindings
    Why didn't i do this in the first place?

--- .config/nvim/lua/vscode/plugins/harpoon.lua
+++ .config/nvim/lua/vscode/plugins/harpoon.lua
@@ -1,33 +1,31 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
+	enabled = true,
 	dependencies = { "nvim-lua/plenary.nvim" },
-	lazy = false,
 	config = function()
 		local harpoon = require("harpoon")
 		-- REQUIRED
 		harpoon:setup()
 		-- REQUIRED
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():append()
-		end)
-		vim.keymap.set("n", "<leader><leader>", function()
-			harpoon.ui:toggle_quick_menu(require("harpoon"):list())
-		end)
-		vim.keymap.set("n", "<leader>q", function()
-			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<leader>w", function()
-			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<leader>e", function()
-			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<leader>r", function()
-			harpoon:list():select(4)
-		end)
-		vim.keymap.set("n", "<leader>t", function()
-			harpoon:list():select(5)
-		end)
+		--stylua: ignore start
+		vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(require("harpoon"):list()) end, { desc = "Harpoon list" })
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon add" })
+		for i = 1,9 do
+			vim.keymap.set("n", "<leader>"..i, function() harpoon:list():select(i) end, { desc = "Harpoon file "..i })
+		end
+		--stylua: ignore end
+
+		-- hotkeys when harpoon window is open only
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "harpoon" },
+			callback = function()
+				for i = 1, 9 do
+					--stylua: ignore start
+						vim.keymap.set("n", tostring(i), function() harpoon:list():select(i) end, { buffer = true })
+					--stylua: ignore end
+				end
+			end,
+		})
 	end,
 }
