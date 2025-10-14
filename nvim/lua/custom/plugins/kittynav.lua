0bb1329fb6066145b2109be211055352f479cd9e
lebedevv <sintrael@gmail.com>
2025-10-14 10:27:21 +0300

    kitty: fix nav

--- nvim/lua/custom/plugins/kittynav.lua
+++ nvim/lua/custom/plugins/kittynav.lua
@@ -1,15 +1,23 @@
--- https://github.com/MunsMan/kitty-navigator.nvim
-return {
-  {
-    -- NOTE this bind ctrl keys by default
-    "MunsMan/kitty-navigator.nvim",
-    opts = { keybindings = {}, },
-  }
-}
-
-
 -- --pane navigation (for kitty nvim integration)
 -- vim.keymap.set({ "n", "v", "x" }, "<C-h>", "<C-w>h")
 -- vim.keymap.set({ "n", "v", "x" }, "<C-j>", "<C-w>j")
 -- vim.keymap.set({ "n", "v", "x" }, "<C-k>", "<C-w>k")
 -- vim.keymap.set({ "n", "v", "x" }, "<C-l>", "<C-w>l")
+
+-- keys = {
+--   { "<C-h>", function() require("kitty-navigator").navigateLeft() end, desc = "Move left a Split", mode = { "n" } },
+--   { "<C-j>", function() require("kitty-navigator").navigateDown() end, desc = "Move down a Split", mode = { "n" } },
+--   { "<C-k>", function() require("kitty-navigator").navigateUp() end,  desc = "Move up a Split",    mode = { "n" } },
+--   { "<C-l>", function() require("kitty-navigator").navigateRight() end, desc = "Move right a Split", mode = { "n" } },
+-- },
+return {
+  -- https://github.com/MunsMan/kitty-navigator.nvim
+  {
+    "MunsMan/kitty-navigator.nvim",
+    build = {
+      "cp navigate_kitty.py ~/.config/kitty",
+      "cp pass_keys.py ~/.config/kitty",
+    },
+    opts = { keybindings = {} },
+  }
+}
