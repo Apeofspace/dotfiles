553e687903a9e350a3e80e846912fed9793721ea
lebedevv <sintrael@gmail.com>
2025-12-29 09:36:58 +0300

    kitty: theme

--- nvim/lua/main_profile/plugins/treesitter.lua
+++ nvim/lua/main_profile/plugins/treesitter.lua
@@ -30,35 +30,6 @@ local M = {
       })
     end,
   },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   build = ":TSUpdate",
-  --   branch = "master", -- deprecated old branch
-  --   config = function()
-  --     ---@diagnostic disable-next-line: missing-fields
-  --     require("nvim-treesitter.configs").setup({
-  --       ensure_installed = {
-  --         "bash",
-  --         "c",
-  --         "html",
-  --         "lua",
-  --         "markdown",
-  --         "markdown_inline",
-  --         "vim",
-  --         "vimdoc",
-  --         "python",
-  --         "go",
-  --         "diff",
-  --         "regex",
-  --       },
-  --       auto_install = true,
-  --       sync_install = true,
-  --       highlight = { enable = true },
-  --       indent = { enable = true },
-  --       -- fold = { enable = false }
-  --     })
-  --   end,
-  -- },
   {
     -- jumping between the matching opposing end of a Tree-sitter node, such as brackets, quotes, and more.
     "yorickpeterse/nvim-tree-pairs",
