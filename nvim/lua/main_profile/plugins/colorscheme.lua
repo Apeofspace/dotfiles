0506074f6d790b5984eadaa5bfa041f15e0f1a7f
lebedevv <sintrael@gmail.com>
2026-03-10 11:25:11 +0300

    nvim: more work on ashen

--- nvim/lua/main_profile/plugins/colorscheme.lua
+++ nvim/lua/main_profile/plugins/colorscheme.lua
@@ -83,31 +83,40 @@ local schemes = {
       local ashen = require("ashen")
       local colors = require("ashen.colors")
       local opts = {
-        colors = { background = "#1d1f21", blue = colors["orange_blaze"] },
+        colors = { background = "#1d1f21", blue = colors["orange_blaze"], g_0 = "#f0f0f0" },
         style_presets = { italic_comments = true },
         hl = {
           merge_override = {
+            ["@constant"] = { "orange_smolder" },
             ["@function"] = { "g_0", { bold = true } },
             ["@function.call"] = { "g_0", { bold = true } },
             ["@function.builtin"] = { "g_0", { bold = true } },
             ["@function.method.call"] = { "g_0", { bold = true } },
             ["@string"] = { "green_light" },
-            ["@operator"] = { "g_6" },
+            ["@operator"] = { "red_ember" },
+            -- ["@operator"] = { "g_6" },
             ["@type"] = { "g_6" },
-            ["@keyword.type"] = { "g_2" },
+            ["@keyword.modifier"] = { "g_2" },
             ["@lsp.type.namespace"] = {},
             ["@lsp.type.property"] = { "g_4" },
-            ["@lsp.type.enum"] = { "grey" },
+            ["@constructor"] = { "orange_glow" },
             ["WinSeparator"] = { "grey" },
-            ["@punctuation.delimiter"] = {"g_5"},
-            ["RenderMarkdownChecked"] = {"green_light"},
+            ["@punctuation.delimiter"] = { "g_5" },
+            ["RenderMarkdownChecked"] = { "green_light" },
+            ["RenderMarkdownCodeInline"] = { "orange_glow" },
           },
           link = {
+            ["@boolean"] = "@constant",
+            ["@number"] = "@constant",
+            ["@number.float"] = "@constant",
+            ["@lsp.type.class"] = "@constructor",
+            ["@lsp.type.enum"] = "@lsp.type.class",
             ["@type.builtin"] = "@type",
             ["@keyword.modifier"] = "@keyword.type",
           },
         },
       }
+      -- ashen.setup({})
       ashen.setup(opts)
     end,
   },
