dd55d63d67b94da5edd6df55601a059c329d28fb
lebedevv <sintrael@gmail.com>
2026-03-16 16:48:08 +0300

    nvim: maplocalleader on md

--- nvim/lua/main_profile/plugins/markdown.lua
+++ nvim/lua/main_profile/plugins/markdown.lua
@@ -143,6 +143,7 @@ local M = {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
+    vim.g.maplocalleader = " "
     vim.opt_local.wrap = true        -- Enable soft line wrapping
     vim.opt_local.linebreak = true   -- Wrap at word boundaries, not in the middle of a word
     vim.opt_local.breakindent = true -- Indent wrapped lines to align with the start of the text
